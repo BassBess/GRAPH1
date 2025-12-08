58095cd8f5894af51b738029290c47a3f16858a7
BassBess <aliezzalrejal7@gmail.com>
2025-12-08 17:20:49 +0200

    Update graph.java

--- graph.java
+++ graph.java
@@ -1,18 +1,41 @@
-public class graph{
+
+import java.util.HashMap;
 
 
-plublic stativ void main(String [] args){
+class graph{
+  
+ private Map<Character,List <Character>> adjlist = new HashMap<>();
 
-System.out.println("hellow worlld")
 
+ public addEdge(Character a,Charachter b){
+   
+        adjlist.putIfAbsent(a, new ArrayList<>());
+        adjlist.get(a).add(b);
+
+ }
+ public addUndirected(Character a, Character b ){
+
+   addVertix(Character a);
+   adjlist.get(a).add(b);
+   addVertix(Character b)
+   adjlist.get(b).add(a);
+
+ }
+
+  
+  public addVertix(Character a){
+    adjlist.putIfAbsent(a, new ArrayList<>()); 
+  }
+
+  public deleteVertix(Character a){
+    
+  for (List<Character> list : adjlist.values()) {
+      list.remove(a)    
+    }    
+    adjlist.remove(a);  
+  }
 
 
   
-}
-
-
-
-
-
 
 }
